25ee990dec363c4edff34ae0f059a09a44b7636d
Adriana <adriana.calvomatos@gmail.com>
2021-09-25 20:45:00 +0000

    refactor async test with M&C

--- test/asyncBreedsTest.js
+++ test/asyncBreedsTest.js
@@ -6,14 +6,14 @@ describe('#breedDetailsFromFile', () => {
     breedDetailsFromFile('Bombay', (bombay) => {
       const expectedDesc =
         "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.\n";
-      assert.equal(expectedDesc, bombay);
+      assert.equal(bombay, expectedDesc);
       done();
     });
   });
 
   it('provides, via callback, undefined for a breed that does not exist', (done) => {
     breedDetailsFromFile('Saphire', (desc) => {
-      assert.equal(undefined, desc);
+      assert.equal(desc, undefined);
       done();
     });
   });
