7bb166db629bc969c999a768863776a6634a33b1
Adriana <adriana.calvomatos@gmail.com>
2021-09-25 20:44:21 +0000

    refactor async test with M&C

---
+++ test/asyncBreedsTest.js
@@ -0,0 +1,24 @@
+const assert = require('chai').assert;
+const breedDetailsFromFile = require('../asyncBreeds');
+
+describe('#breedDetailsFromFile', () => {
+  it('provides, via callback, breed details for the Bombay breed', (done) => {
+    breedDetailsFromFile('Bombay', (bombay) => {
+      const expectedDesc =
+        "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.\n";
+      assert.equal(expectedDesc, bombay);
+      done();
+    });
+  });
+
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+      assert.equal(undefined, desc);
+      done();
+    });
+  });
+});
+
+// line 8 error: was not matching bomby actual in line 9 assertion. Actual ends with \n due to being console.log
+//     solution: tried using process.stdout.write in asyncBreeds.js so the output would not contain trailing \n but could not get it to work
+//               instead, added \n to expected
